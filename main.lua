ace6103f29d61b837d148cf15c9cbb5493b0ac8f
James Bradbury <jamesbradbury93@gmail.com>
2020-09-30 17:58:12 +0100

    1.3.7

--- main.lua
+++ main.lua
@@ -15,10 +15,10 @@ RF.myRealm = string.gsub(spaced_realm, "'", "")
 
 RF:setRegionRealmLabel(RF.myRealm)
 
----- Labelling and Removing Functions
+---- Removing Enrties when togRemove is enabled
 function RF.removeEntries(results)
-	if RF.togRemove == true then
-		for idx = #results, 1, -1 do
+	if RF.togRemove then
+		for idx=1, #results  do
 			local resultID = results[idx]
 			local searchResults = C_LFGList.GetSearchResultInfo(resultID)
 			local activitiyID1 = searchResults.activityID
@@ -29,44 +29,17 @@ function RF.removeEntries(results)
 				local info = servers[realm]
 				if info ~= nil then
 					local region = info[1]
-		
-					if RF.region == 'NA' then -- if your own region is NA colour code NA servers appropriately
-						if region == 'NA' then
-							-- Do nothing because its in your region
-						else
-							table.remove(results, idx)
-						end
-					end
-
-					if RF.region == 'BR' then -- if your own region is NA colour code BR servers appropriately
-						if region == 'BR' then
-						else
-							table.remove(results, idx)
-						end
-					end
-
-					if RF.region == 'LA' then -- if your own region is NA colour code LA servers appropriately
-						if region == 'LA' then
-						else
-							table.remove(results, idx)
-						end
-					end
-
-					if RF.region == 'OC' then -- if your own region is NA colour code OC servers appropriately
-						if region == 'OC' then
-						else
-							table.remove(results, idx)
-						end
+					if RF.region ~= region then
+						table.remove(results, idx)
 					end
 				end
 			end
+			table.sort(results)
 		end
-	-- If the flag is switched off
-	elseif RF.togRemove == false then
-		-- do nothing!
 	end
 end
 
+---- Updating the text of entries
 function RF.updateEntries(results)
 	local searchResults = C_LFGList.GetSearchResultInfo(results.resultID)
 	local activityID = searchResults.activityID
@@ -83,20 +56,20 @@ function RF.updateEntries(results)
 				if region == 'NA' then
 					if dataCentre == 'NYC' then
 						if RF.dataCentre == dataCentre then -- if your personal data centre matches the queried leader/realm name (colour coding for home data centres)
-							results.ActivityName:SetText ("|cFF00CCFF["..cat.na_nyc_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFF00CCFF["..cat.na_nyc_id.."]|r " .. activityName)
 						else
-							results.ActivityName:SetText ("|cFFFFFF00["..cat.na_nyc_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFFFFFF00["..cat.na_nyc_id.."]|r " .. activityName)
 						end
-						results.ActivityName:SetTextColor (0, 1, 0)
+						results.ActivityName:SetTextColor(0, 1, 0)
 					end
 
 					if dataCentre == 'PHX' then
 						if RF.dataCentre == dataCentre then
-							results.ActivityName:SetText ("|cFF00CCFF["..cat.na_phx_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFF00CCFF["..cat.na_phx_id.."]|r " .. activityName)
 						else
-							results.ActivityName:SetText ("|cFFFFFF00["..cat.na_phx_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFFFFFF00["..cat.na_phx_id.."]|r " .. activityName)
 						end
-						results.ActivityName:SetTextColor (0, 1, 0)
+						results.ActivityName:SetTextColor(0, 1, 0)
 					end
 
 					if dataCentre == 'LA' then
@@ -105,47 +78,38 @@ function RF.updateEntries(results)
 						else
 							results.ActivityName:SetText ("|cFFFFFF00["..cat.na_la_id.."]|r " .. activityName)
 						end
-						results.ActivityName:SetTextColor (0, 1, 0)
+						results.ActivityName:SetTextColor(0, 1, 0)
 					end
 
 					if dataCentre == 'CHI' then
 						if RF.dataCentre == dataCentre then
-							results.ActivityName:SetText ("|cFF00CCFF["..cat.na_chi_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFF00CCFF["..cat.na_chi_id.."]|r " .. activityName)
 						else
-							results.ActivityName:SetText ("|cFFFFFF00["..cat.na_chi_id.."]|r " .. activityName)
+							results.ActivityName:SetText("|cFFFFFF00["..cat.na_chi_id.."]|r " .. activityName)
 						end
-						results.ActivityName:SetTextColor (0, 1, 0)
+						results.ActivityName:SetTextColor(0, 1, 0)
 					end
 				end
 			end
 
 			-- non-NA realms
-			if RF.region == 'BR' then
-				if region == 'BR' then
-					results.ActivityName:SetText ("|cFFFFFF00["..cat.br.."]|r " .. activityName)
-					results.ActivityName:SetTextColor (0, 1, 0)
-				end
+			if RF.region == 'BR' and region == 'BR' then
+				results.ActivityName:SetText("|cFFFFFF00["..cat.br.."]|r " .. activityName)
+				results.ActivityName:SetTextColor(0, 1, 0)
 			end
 
-			if RF.region == 'LA' then
-				if region == 'LA' then		
-					results.ActivityName:SetText ("|cFFFFFF00["..cat.la.."]|r " .. activityName)
-					results.ActivityName:SetTextColor (0, 1, 0)	
-				end
+			if RF.region == 'LA' and region == 'LA' then
+				results.ActivityName:SetText("|cFFFFFF00["..cat.la.."]|r " .. activityName)
+				results.ActivityName:SetTextColor(0, 1, 0)	
 			end
 			
-			if RF.region == 'OC' then
-				if region == 'OC' then
-					results.ActivityName:SetText ("|cFFFFFF00["..cat.oc.."]|r " .. activityName)
-					results.ActivityName:SetTextColor (0, 1, 0)	
-				end
+			if RF.region == 'OC' and region == 'OC' then
+				results.ActivityName:SetText("|cFFFFFF00["..cat.oc.."]|r " .. activityName)
+				results.ActivityName:SetTextColor(0, 1, 0)	
 			end
 		end
 	end
 end
-
-hooksecurefunc ("LFGListSearchEntry_Update", 	 RF.updateEntries)
-hooksecurefunc ("LFGListUtil_SortSearchResults", RF.removeEntries)
 
 
 SLASH_RFILTER1 = "/rfilter"
@@ -166,7 +130,10 @@ local welcomePrompt = CreateFrame("Frame")
 welcomePrompt:RegisterEvent("PLAYER_LOGIN")
 welcomePrompt:SetScript("OnEvent", function(_, event)
 	if event == "PLAYER_LOGIN" then
-		print("|cff00ffff[Region Filter]|r |cffffcc00Version 1.3.6|r. If there any bugs please report them via https://wow.curseforge.com/projects/regionfilter or https://github.com/jamesb93/RegionFilter")
+		print("|cff00ffff[Region Filter]|r |cffffcc00Version 1.3.7|r. If there any bugs please report them via https://wow.curseforge.com/projects/regionfilter or https://github.com/jamesb93/RegionFilter")
 		print(RF.postType)
 	end
 end)
+
+hooksecurefunc("LFGListUtil_SortSearchResults", RF.removeEntries)
+hooksecurefunc("LFGListSearchEntry_Update", 	RF.updateEntries)
--- utils.lua
+++ utils.lua
@@ -3,20 +3,20 @@ local RF = select(2, ...)
 function RF:sanitiseName(leaderName)
 	-- returns name, realm when passed a name-realm full name
 	if string.match(leaderName, "-") then
-		local t_name, t_realm = strsplit("-", leaderName, 2)
-		local t_realm = string.gsub(t_realm, "'", "")
-		return t_name, t_realm
+		local name, realm = strsplit("-", leaderName, 2)
+		local realm = realm:gsub("'", "")
+		return name, realm
 	else
 		
-		local t_name = leaderName
-		local t_realm = RF.myRealm
-		return t_name, t_realm
+		local name = leaderName
+		local realm = RF.myRealm
+		return name, realm
 	end
 end
 
 function RF:splitName(leaderName)
 	-- returns name, realm when passed a name-realm full name
-	local t_name, t_realm = strsplit("-", leaderName, 2)
-	local t_realm = string.gsub(t_realm, "'", "")
-	return t_name, t_realm
+	local name, realm = strsplit("-", leaderName, 2)
+	local realm = realm:gsub("'", "")
+	return name, realm
 end
