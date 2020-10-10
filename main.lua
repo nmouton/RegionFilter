a01885902eb1b170267ead14c80c20b26b6e2ca8
James Bradbury <jamesbradbury93@gmail.com>
2020-10-10 16:12:42 +0100

    version 1.3.8

--- main.lua
+++ main.lua
@@ -130,7 +130,7 @@ local welcomePrompt = CreateFrame("Frame")
 welcomePrompt:RegisterEvent("PLAYER_LOGIN")
 welcomePrompt:SetScript("OnEvent", function(_, event)
 	if event == "PLAYER_LOGIN" then
-		print("|cff00ffff[Region Filter]|r |cffffcc00Version 1.3.7|r. If there any bugs please report them at https://github.com/jamesb93/RegionFilter")
+		print("|cff00ffff[Region Filter]|r |cffffcc00Version 1.3.8|r. If there any bugs please report them at https://github.com/jamesb93/RegionFilter")
 		print("If possible, stop using CurseForge (soon/now to be Overwolf) and try CurseBreaker https://www.github.com/AcidWeb/CurseBreaker.")
 		print(RF.postType)
 	end
